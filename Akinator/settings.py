b2e49ab60d72ded6c3345f4dbe41b8ca8a45d960
RedAlf121 <francismirezDPL@gmail.com>
2025-09-07 18:51:07 -0400

    fix: turn of debug and add all allowed hosts

--- Akinator/settings.py
+++ Akinator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i9qw**tb0mlmzu_h)k6g+v$u0idw4ay3ztu&^ig42q^n8#-gjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
